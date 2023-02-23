9e9ba0874a5be9716ea7119b3c86f824281f08c2
amaraimusi <amaraimusi@gmail.com>
2023-02-23 16:29:28 +0900

    mp4ファイル名一括変更

--- src/file_rename.py
+++ src/file_rename.py
@@ -14,7 +14,7 @@ import os
 import datetime
 
 # フォーマット済みの現在日付を取得
-now = datetime.datetime.now().strftime("%Y%m%d")
+#now = datetime.datetime.now().strftime("%Y%m%d")
 
 # 対象ディレクトリのパス
 target_dir = configs['target_dir'];
@@ -23,8 +23,15 @@ target_dir = configs['target_dir'];
 for filename in os.listdir(target_dir):
     # mp4ファイルのみ処理対象とする
     if filename.endswith(".mp4"):
+        
+        mtime = os.path.getmtime(os.path.join(target_dir, filename))
+        #update_date = datetime.datetime.fromtimestamp(mtime).strftime("%Y/%m/%d %H:%M:%S")
+        update_date = datetime.datetime.fromtimestamp(mtime).strftime("%m%d")
+        
         # ファイル更新日を付け足した新しいファイル名を生成
-        new_filename = f"{filename[:-4]}_{now}.mp4"
+        new_filename = f"{update_date}_{filename[:-4]}.mp4"
+        
+        print(new_filename)
         # ファイル名を変更
         os.rename(os.path.join(target_dir, filename), os.path.join(target_dir, new_filename))
 
