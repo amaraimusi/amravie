ed2893829174886c6ea709080f1e442ad082bd20
amaraimusi <amaraimusi@gmail.com>
2023-06-12 15:42:51 +0900

    ファイル名変更ツールを修正

--- src/file_rename.py
+++ src/file_rename.py
@@ -22,18 +22,21 @@ target_dir = configs['target_dir'];
 # ディレクトリ内のファイルを走査
 for filename in os.listdir(target_dir):
     # mp4ファイルのみ処理対象とする
-    if filename.endswith(".mp4"):
+    # if filename.endswith(".mp4"):
+    
+    # 拡張子を取得する
+    ext = os.path.splitext(filename)[1]
         
-        mtime = os.path.getmtime(os.path.join(target_dir, filename))
-        #update_date = datetime.datetime.fromtimestamp(mtime).strftime("%Y/%m/%d %H:%M:%S")
-        update_date = datetime.datetime.fromtimestamp(mtime).strftime("%m%d")
-        
-        # ファイル更新日を付け足した新しいファイル名を生成
-        new_filename = f"{update_date}_{filename[:-4]}.mp4"
-        
-        print(new_filename)
-        # ファイル名を変更
-        os.rename(os.path.join(target_dir, filename), os.path.join(target_dir, new_filename))
+    mtime = os.path.getmtime(os.path.join(target_dir, filename))
+    #update_date = datetime.datetime.fromtimestamp(mtime).strftime("%Y/%m/%d %H:%M:%S")
+    update_date = datetime.datetime.fromtimestamp(mtime).strftime("%y_%m%d")
+    
+    # ファイル更新日を付け足した新しいファイル名を生成
+    new_filename = f"{update_date}_{filename[:-4]}{ext}"
+    
+    print(new_filename)
+    # ファイル名を変更
+    os.rename(os.path.join(target_dir, filename), os.path.join(target_dir, new_filename))
 
 
 print('Success')
